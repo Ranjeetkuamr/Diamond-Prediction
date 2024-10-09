4abd32f6b5d2bf3e91bbb70a8e78ccd8b9b95287
Ranjeetkuamr <ranjeetk5498@gmail.com>
2024-10-10 02:54:25 +0530

    dvc commit

--- setup.py
+++ setup.py
@@ -19,10 +19,10 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='diamonpredict',
 version='0.0.1',
-author='krish',
-author_email='krishnaik06@gmail.com',
+author='ranjeetkumar',
+author_email='ranjeetk5498@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 )
